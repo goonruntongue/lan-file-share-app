6341a1a9f4a33a26283be0c31a1cb73eed4eb0e5
goonruntongue <goonruntongue@gmail.com>
2026-07-12 15:43:37 +0900

    Revert "Refresh PWA cache on update"
    This reverts commit 6f99dbc79d0829ae056327dace239157908829ae.

--- pwa/sw.js
+++ pwa/sw.js
@@ -1,6 +1,6 @@
-const CACHE = "lan-file-share-pwa-v3";
+const CACHE = "lan-file-share-pwa-v2";
 const ASSETS = ["/", "/index.html", "/styles.css", "/app.js", "/config.js", "/manifest.webmanifest", "/icons/icon.svg", "/icons/icon-192.png", "/icons/icon-512.png"];
-self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
+self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
 self.addEventListener("activate", event => event.waitUntil(self.clients.claim()));
 self.addEventListener("fetch", event => {
   if (new URL(event.request.url).origin === location.origin) event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request)));
